580d4d65d6aeac222e31fad693db78c7175bf9c2
Stephanie Preuthen <kann.zaubern@gmail.com>
2025-01-22 18:49:43 +0100

    enabling deletion of a color

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,17 +1,34 @@
 import "./Color.css";
+import { useState } from "react";
 
-export default function Color({ color }) {
+export default function Color({ color, onDeleteColor }) {
+  const [confirmationMessage, setConfirmationMessage] = useState("Delete"); // State to handle confirmation message
+
+  function handleConfirmation() {
+    setConfirmationMessage("Are you sure?");
+  }
+
   return (
     <div
       className="color-card"
       style={{
-        background: color.hex, // setting background color of card
-        color: color.contrastText, // setting text color for card
+        background: color.hex,
+        color: color.contrastText,
       }}
     >
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+
+      {/* Delete Button */}
+      <button className="delete-button" onClick={() => handleConfirmation()}>
+        {confirmationMessage}
+      </button>
+
+      {/* Confirmation Button */}
+      {confirmationMessage == "Are you sure?" && (
+        <button onClick={() => onDeleteColor(color.id)}>Yes</button>
+      )}
     </div>
   );
 }
